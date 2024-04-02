7464ac71a7b89591f61513a4b5bd7670b350c9ba
FlareYun <flaregameryt@gmail.com>
2024-04-01 23:38:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
+app.all('*', (req, res) => {
     
     // Check if the file exists
 
