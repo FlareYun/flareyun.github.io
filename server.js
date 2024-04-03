338631f67feee90864bc53492da2252c9b0cb1fc
FlareYun <flaregameryt@gmail.com>
2024-04-02 23:49:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 443;
 
 app.all('*', (req, res) => {
     
